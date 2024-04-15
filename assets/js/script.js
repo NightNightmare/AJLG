31e6875c3a0676cb0621f15f0a86653c572fc9bc
Ângelo Vaz <angelovaz2001@gmail.com>
2024-04-15 19:16:58 -0100

    scroll top button

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,29 @@ function showMenu() {
         document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
     }
 }
+
+//animação e função do "botão de scroll para o top com progresso do scroll"
+
+let calcScrollValue = () => {
+    let scrollProgress = document.getElementById("progresso");
+    let progressValue = document.getElementById("valor-progresso");
+    let pos = document.documentElement.scrollTop;
+    //console.log(pos); para pegar o valor da tela
+    let calcHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    //console.log(calcHeight) pra saber a altura exata k precisamos
+    let scrollValue = Math.round((pos * 100) / calcHeight);
+    if(pos > 100) {
+        scrollProgress.style.display = "grid";
+    } else {
+        scrollProgress.style.display = "none";
+    }
+
+    scrollProgress.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+
+    scrollProgress.style.background = 'conic-gradient(#7f37c9 $[scrollValue]%, #d7d7d7 ${scrollValue}%)';
+}
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
